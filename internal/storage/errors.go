f9f3d0363eda21c2976c94fc17c506f4f830bdee
FinnTew <nanomoa76@gmail.com>
2025-01-24 12:33:37 +0800

    add some error definition

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrKeyNotFound = errors.New("key not found")
+	ErrNilKey      = errors.New("key is nil")
 	ErrDBClosed    = errors.New("database is closed")
 	// TODO: add more errors here
 )
