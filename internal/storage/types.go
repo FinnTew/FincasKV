2b7bb09926763ce7703108f9b01a4a8f265633a3
FinnTew <nanomoa76@gmail.com>
2025-01-25 17:23:31 +0800

    add Record, Entry, DataFile type definition

--- internal/storage/types.go
+++ internal/storage/types.go
@@ -1,10 +1,35 @@
 package storage
 
-type BitcaskItem[K comparable, V any] struct {
-	Key   K
-	Value V
+import (
+	"os"
+	"sync"
+	"sync/atomic"
+)
+
+type KVItem struct {
+	Key   []byte
+	Value []byte
+}
+
+type Record struct {
+	Timestamp int64
+	Checksum  uint64
+	Flags     uint32
+	KVItem
 }
 
 type Entry struct {
-	// TODO: add fields
+	FileID    int
+	Offset    int64
+	Size      uint32
+	Timestamp int64
+}
+
+type DataFile struct {
+	ID     int
+	Path   string
+	File   *os.File
+	Offset atomic.Int64
+	Closed atomic.Bool
+	mu     sync.Mutex
 }
