32d525c9eb2b5ef0cd8df81e215c8465727f9184
FinnTew <nanomoa76@gmail.com>
2025-01-26 18:13:22 +0800

    update

--- cmd/fincas/main.go
+++ cmd/fincas/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/FinnTew/FincasKV/internal/storage"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -19,25 +20,40 @@ func main() {
 	defer db.Close()
 
 	// 写入数据
-	if err := db.Put("key1", []byte("value1")); err != nil {
-		log.Fatal(err)
+	for i := 1; i <= 1000; i++ {
+		if err := db.Put("key"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i))); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	// 读取数据
-	value, err := db.Get("key1")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(string(value))
+	for i := 1; i <= 1000; i++ {
+		value, err := db.Get("key" + strconv.Itoa(i))
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println(string(value))
+		}
 	}
 
 	// 删除数据
-	if err := db.Del("key1"); err != nil {
-		log.Fatal(err)
+	for i := 1; i <= 1000; i += 2 {
+		if err := db.Del("key" + strconv.Itoa(i)); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	// 合并文件
 	if err := db.Merge(); err != nil {
 		log.Fatal(err)
+	}
+
+	for i := 1; i <= 1000; i++ {
+		value, err := db.Get("key" + strconv.Itoa(i))
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(value))
+		}
 	}
 }
