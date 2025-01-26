59e3f24d1d29a59d9199245b5dca0d580c3a101b
FinnTew <nanomoa76@gmail.com>
2025-01-26 17:58:11 +0800

    add usage example

--- cmd/fincas/main.go
+++ cmd/fincas/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/FinnTew/FincasKV/internal/storage"
+	"log"
+)
 
 func main() {
-	fmt.Println("FincasKV")
+	// 创建数据库实例
+	db, err := storage.Open(
+		storage.WithDataDir("./data"),
+		storage.WithMaxFileSize(1<<30),  // 1GB
+		storage.WithMemCacheSize(1<<20), // 1MB
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 写入数据
+	if err := db.Put("key1", []byte("value1")); err != nil {
+		log.Fatal(err)
+	}
+
+	// 读取数据
+	value, err := db.Get("key1")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(string(value))
+	}
+
+	// 删除数据
+	if err := db.Del("key1"); err != nil {
+		log.Fatal(err)
+	}
+
+	// 合并文件
+	if err := db.Merge(); err != nil {
+		log.Fatal(err)
+	}
 }
