4f39d74439cdc797cec5a400b274c76d3f79afa0
FinnTew <nanomoa76@gmail.com>
2025-01-27 21:09:27 +0800

    update usage

--- cmd/fincas/main.go
+++ cmd/fincas/main.go
@@ -2,65 +2,58 @@ package main
 
 import (
 	"fmt"
+	"github.com/FinnTew/FincasKV/internal/database/base"
+	"time"
+
 	"github.com/FinnTew/FincasKV/internal/storage"
-	"github.com/FinnTew/FincasKV/internal/storage/bitcask"
-	"log"
-	"strconv"
 )
 
 func main() {
-	// 创建数据库实例
-	db, err := bitcask.Open(
+	opts := base.DefaultBaseDBOptions()
+	opts.ExpireCheckInterval = 30 * time.Second
+	opts.FlushTTLOnChange = false
+	opts.TTLMetadataFile = "my_ttl_data.txt"
+
+	db, err := base.NewDB(opts,
 		storage.WithDataDir("./data"),
-		storage.WithMaxFileSize(1<<30),  // 1GB
-		storage.WithMemCacheSize(1<<20), // 1MB
-		storage.WithMemIndexDS(storage.BTree),
-	)
+		storage.WithMaxFileSize(32*1024))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer db.Close()
 
-	// 写入数据
-	for i := 1; i <= 1000; i++ {
-		if err := db.Put("key"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i))); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// 读取数据
-	for i := 1; i <= 1000; i++ {
-		value, err := db.Get("key" + strconv.Itoa(i))
-		if err != nil {
-			log.Fatal(err)
-		} else {
-			fmt.Println(string(value))
-		}
-	}
-
-	// 删除数据
-	for i := 1; i <= 1000; i += 2 {
-		if err := db.Del("key" + strconv.Itoa(i)); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// 合并文件
-	if err := db.Merge(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 1; i <= 1000; i++ {
-		value, err := db.Get("key" + strconv.Itoa(i))
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(string(value))
-		}
-	}
-
-	err = db.Close()
+	err = db.Put("hello", "world")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	val, err := db.Get("hello")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hello =>", val)
+
+	err = db.Expire("hello", 3*time.Second)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(4 * time.Second) // 等过期
+
+	exists, err := db.Exists("hello")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("exists(hello) after TTL:", exists) // false
+
+	_ = db.Put("test1", "foo")
+	_ = db.Put("test2", "bar")
+	keys, _ := db.Keys("test*")
+	fmt.Println("keys(test*):", keys) // [test1 test2] 顺序未定
+
+	t, _ := db.Type("test1")
+	fmt.Println("Type of test1:", t) // string
+	t, _ = db.Type("non-exists")
+	fmt.Println("Type of non-exists:", t) // none
+
+	_ = db.Expire("test1", 10*time.Second)
+	db.Persist("test1")
 }
