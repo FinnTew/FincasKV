f06dcecf444d472a2c7a12b764aad24214ab9882
FinnTew <nanomoa76@gmail.com>
2025-01-29 22:03:44 +0800

    designed the key of redis structure

--- internal/database/redis/base.go
+++ internal/database/redis/base.go
@@ -4,20 +4,67 @@ import (
 	"fmt"
 	"github.com/FinnTew/FincasKV/internal/database/base"
 	"github.com/FinnTew/FincasKV/internal/storage"
+	"math"
 	"sync"
 	"time"
 )
 
 var (
-	StringPrefix = "string:"
-	HashPrefix   = "hash:"
-	ListPrefix   = "list:"
-	SetPrefix    = "set:"
-	ZSetPrefix   = "zset:"
+	StringPrefix = "string"
+	HashPrefix   = "hash"
+	ListPrefix   = "list"
+	SetPrefix    = "set"
+	ZSetPrefix   = "zset"
 )
 
 func GetStringKey(key string) string {
-	return fmt.Sprintf("%s%s", StringPrefix, key)
+	return fmt.Sprintf("%s:%s", StringPrefix, key)
+}
+
+func GetHashFieldKey(key, field string) string {
+	return fmt.Sprintf("%s:%s:%s", HashPrefix, key, field)
+}
+
+func GetListItemKey(key string, index int64) string {
+	return fmt.Sprintf("%s:%s:%d", ListPrefix, key, index)
+}
+
+func GetListLenKey(key string) string {
+	return fmt.Sprintf("%s:%s:_len_", ListPrefix, key)
+}
+
+func GetListHeadKey(key string) string {
+	return fmt.Sprintf("%s:%s:_head_", ListPrefix, key)
+}
+
+func GetListTailKey(key string) string {
+	return fmt.Sprintf("%s:%s:_tail_", ListPrefix, key)
+}
+
+func GetSetMemberKey(key, member string) string {
+	return fmt.Sprintf("%s:%s:%s", SetPrefix, key, member)
+}
+
+func GetSetLenKey(key string) string {
+	return fmt.Sprintf("%s:%s:_len_", SetPrefix, key)
+}
+
+func GetZSetMemberScoreKey(key, member string) string {
+	return fmt.Sprintf("%s:%s:%s", ZSetPrefix, key, member)
+}
+
+func float64ToOrderedString(score float64) string {
+	bits := math.Float64bits(score)
+	if (bits & (1 << 63)) != 0 {
+		bits ^= 0xFFFFFFFFFFFFFFFF
+	} else {
+		bits |= 1 << 63
+	}
+	return fmt.Sprintf("%016x", bits)
+}
+
+func GetZSetSortKey(key string, score float64, member string) string {
+	return fmt.Sprintf("%s:%s:s:%s:%s", ZSetPrefix, key, float64ToOrderedString(score), member)
 }
 
 var bitcaskOpts = []storage.Option{
